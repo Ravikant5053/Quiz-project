08ca5744d08d0122caa6179fbe8f534c807e1dea
Ravikant5053 <161645263+Ravikant5053@users.noreply.github.com>
2024-04-19 18:38:35 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const quizData = [
     {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Madrid"],
-      answer: "Paris"
+      question: "How much marks i will get in this project out of 20?",
+      options: ["20", "21", "69", "88"],
+      answer: "21"
     },
     {
-      question: "What is 2 + 2?",
-      options: ["3", "4", "5", "6"],
-      answer: "4"
+      question: "Who penned the book 'Wing of Fire'?",
+      options: ["APJ Abdul Kalam", "Narpender", "RaviKant", "Anchita Ma'am"],
+      answer: "APJ Abdul Kalam"
     },
     {
       question: "Which is the largest planet in our solar system?",
